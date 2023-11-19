c0b5b05bd03ad79203e31ae32a250176471918ae
Xenia <xentimoshenko@gmail.com>
2023-11-19 18:31:07 +0300

    Initial commit:      - основная структура проекта      - необходимые зависимости      - gitignore

---
+++ src/main/java/com/testtaskt1/TestTaskT1Application.java
@@ -0,0 +1,13 @@
+package com.testtaskt1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskT1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTaskT1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/testtaskt1/TestTaskT1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testtaskt1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskT1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
